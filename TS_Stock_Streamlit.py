34c48e4e316d70d9470b72a104528e320ab0383f
klamsal <kamallamsal@gmail.com>
2024-11-12 11:15:08 -0600

    Update TS_Stock_Streamlit.py

--- TS_Stock_Streamlit.py
+++ TS_Stock_Streamlit.py
@@ -28,16 +28,16 @@ if user_ticker:
             if forecast_method == "Simple Moving Average":
                 # Calculate Simple Moving Average (SMA)
                 stock_data["SMA"] = stock_data["Close"].rolling(window=window_size).mean()
-                stock_data = stock_data.dropna(subset=["SMA"])
+                stock_data = stock_data.dropna(subset=["Close", "SMA"])
                 st.write(f"Simple Moving Average with Window Size {window_size}")
-                st.line_chart(stock_data["Close"].combine_first(stock_data["SMA"].rename("SMA")))
+                st.line_chart(stock_data[["Close", "SMA"]])
 
             elif forecast_method == "Exponential Moving Average":
                 # Calculate Exponential Moving Average (EMA)
                 stock_data["EMA"] = stock_data["Close"].ewm(span=window_size, adjust=False).mean()
-                stock_data = stock_data.dropna(subset=["EMA"])
+                stock_data = stock_data.dropna(subset=["Close", "EMA"])
                 st.write(f"Exponential Moving Average with Span {window_size}")
-                st.line_chart(stock_data["Close"].combine_first(stock_data["EMA"].rename("EMA")))
+                st.line_chart(stock_data[["Close", "EMA"]])
 
             # Plotting the Chart
             fig, ax = plt.subplots()
